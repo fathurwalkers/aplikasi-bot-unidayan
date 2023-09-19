cd87bf066b98bfc803eb60340acc36ed2057910b
Fathur Walkers <fathurwalkers44@gmail.com>
2023-09-19 01:34:10 -0700

    update

--- app.py
+++ app.py
@@ -5,13 +5,29 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f"""
+    Selamat Datang {update.effective_user.first_name}, di Bot Sistem Informasi Informatika Unidayan!
+    Silahkan gunakan perintah " /help " untuk melihat apa saja Command (perintah) yang tersedia pada Bot ini untuk melihat beberapa informasi terkait informasi dan data akademik pada Fakultas Teknik Informatika Universitas Dayanu Ikhsanuddin.""")
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("""
+    await update.message.reply_text(
+    """
     /start      -> Displaying welcome message. 
-    /help       -> Get information of basic usage of this bot. 
-    /contact    -> Contact information.""")
+    ..
+    /help       -> Get information of basic usage of this bot.
+    .. 
+    /jadwalmatkul   -> Mengambil informasi terkait matakuliah. 
+    ..
+    /transkip   -> Mengambil data Transkip nilai sesuai dengan NIM yang di request. 
+    ex : /transkip 2020 20650001
+    ..
+    /khs        -> Mengambil data Kartu Hasil Studi sesuai dengan NIM yang di request. 
+    ex : /khs 2020 20650001
+    ..
+    /krs        -> Mengambil data KRS pada semester yang aktif. 
+    ex : /krs 
+    ..
+    """)
 
 async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"""Instagram : sir.n3wt0n_ Current User : {update.effective_user.id} 
@@ -28,7 +44,7 @@ async def sendfile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     tmp_filename = 'ex_doc/' + params + '.pdf'
     document = open(tmp_filename, 'rb')
     await update.message.reply_document(document=document)
-    
+
 async def transkip(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
     params = "/".join(context.args)
@@ -48,9 +64,15 @@ async def krs(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     tmp_filename = 'ex_doc/Kartu Rencana Studi/KRS MBKM SMT GENAP.pdf'
     document = open(tmp_filename, 'rb')
     await update.message.reply_document(document=document)
+    
+async def jadwalmatkul(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
+    tmp_filename = 'ex_doc/jadwal-matkul/JADWAL PERKULIAHAN SEMESTER GENAP TAHUN AKADEMIK 2022 2023.pdf'
+    document = open(tmp_filename, 'rb')
+    await update.message.reply_document(document=document)
 
 # app = ApplicationBuilder().token("6320416802:AAEFPX1kePXFNt8o-ohSvtzINdsFaNrpOps").build() # FATHUR BOT  
-app = ApplicationBuilder().token("6544544911:AAFYAqDxGq-rdbmZAZMzmnIL-iwR8clV8ak").build() # SISFO UNIDAYAN BOT 
+app = ApplicationBuilder().token("6544544911:AAFYAqDxGq-rdbmZAZMzmnIL-iwR8clV8ak").read_timeout(30).write_timeout(30).build() # SISFO UNIDAYAN BOT 
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help))
@@ -59,6 +81,7 @@ app.add_handler(CommandHandler("sendfile", sendfile))
 app.add_handler(CommandHandler("transkip", transkip))
 app.add_handler(CommandHandler("khs", khs))
 app.add_handler(CommandHandler("krs", krs))
+app.add_handler(CommandHandler("jadwalmatkul", jadwalmatkul))
 app.add_handler(CommandHandler("test", test))
 
 app.run_polling()
