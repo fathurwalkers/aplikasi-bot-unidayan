db035dfc30bcecba4b301660731ff38cd67283b0
Fathur Walkers <fathurwalkers44@gmail.com>
2023-09-18 17:16:05 -0700

    update

--- app.py
+++ app.py
@@ -29,21 +29,36 @@ async def sendfile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     document = open(tmp_filename, 'rb')
     await update.message.reply_document(document=document)
     
-async def tr(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def transkip(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
     params = "/".join(context.args)
     tmp_filename = 'ex_doc/Transkip Nilai/' + params + 'TR.pdf'
     document = open(tmp_filename, 'rb')
     await update.message.reply_document(document=document)
+    
+async def khs(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
+    params = "/".join(context.args)
+    tmp_filename = 'ex_doc/Kartu Hasil Studi/' + params + '.pdf'
+    document = open(tmp_filename, 'rb')
+    await update.message.reply_document(document=document)
+    
+async def krs(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w')
+    tmp_filename = 'ex_doc/Kartu Rencana Studi/KRS MBKM SMT GENAP.pdf'
+    document = open(tmp_filename, 'rb')
+    await update.message.reply_document(document=document)
 
-app = ApplicationBuilder().token("6320416802:AAEFPX1kePXFNt8o-ohSvtzINdsFaNrpOps").build() # FATHUR BOT  
-# app = ApplicationBuilder().token("6544544911:AAFYAqDxGq-rdbmZAZMzmnIL-iwR8clV8ak").build() # SISFO UNIDAYAN BOT 
+# app = ApplicationBuilder().token("6320416802:AAEFPX1kePXFNt8o-ohSvtzINdsFaNrpOps").build() # FATHUR BOT  
+app = ApplicationBuilder().token("6544544911:AAFYAqDxGq-rdbmZAZMzmnIL-iwR8clV8ak").build() # SISFO UNIDAYAN BOT 
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help))
 app.add_handler(CommandHandler("contact", contact))
 app.add_handler(CommandHandler("sendfile", sendfile))
-app.add_handler(CommandHandler("tr", tr))
+app.add_handler(CommandHandler("transkip", transkip))
+app.add_handler(CommandHandler("khs", khs))
+app.add_handler(CommandHandler("krs", krs))
 app.add_handler(CommandHandler("test", test))
 
 app.run_polling()
